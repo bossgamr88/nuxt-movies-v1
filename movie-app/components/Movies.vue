5c1699cbc06768e3e93eced6116311086fec6f93
bossgamr88 <zerogamr11@gmail.com>
2022-03-26 23:52:26 +0700

    search movie done

--- movie-app/components/Movies.vue
+++ movie-app/components/Movies.vue
@@ -7,47 +7,105 @@
     <div v-else>
       <v-container>
         <div class="search">
-          <v-text-field placeholder="Search Here..."></v-text-field>
-          <v-btn style="margin-left:20px;" color="error">Clear</v-btn>
-        </div>
-        <v-row>
-          <v-col
-            xl="3"
-            md="4"
-            sm="6"
-            xs="12"
-            v-for="movie in movies"
-            :key="movie.id"
+          <v-text-field
+            @keyup.enter="$fetch"
+            v-model.lazy="search"
+            placeholder="Search Here..."
+          ></v-text-field>
+          <v-btn
+            v-if="search !== ''"
+            @click="clearSearch"
+            style="margin-left: 20px"
+            color="error"
+            >Clear</v-btn
           >
-            <v-card>
-              <!-- <v-img
+        </div>
+        <div v-if="search === ''">
+          <v-row>
+            <v-col
+              xl="3"
+              md="4"
+              sm="6"
+              xs="12"
+              v-for="movie in movies"
+              :key="movie.id"
+            >
+              <v-card>
+                <!-- <v-img
                 class="image"
                 height="250"
                 src="https://images.unsplash.com/photo-1512070853659-f08f0efae470?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1070&q=80"
               ></v-img> -->
-              <v-img
+                <v-img
+                  class="image"
+                  height="250"
+                  :src="`https://www.themoviedb.org/t/p/w500/${movie.poster_path}`"
+                >
+                </v-img>
+                <v-card-title>
+                  <!-- <h4>Moive title</h4> -->
+                  <!-- <h4>{{ movie.title }}</h4> -->
+                  <h4>
+                    {{ movie.title.slice(0, 10) }}
+                    {{ movie.title.length > 10 ? "....." : "" }}
+                  </h4>
+                </v-card-title>
+                <v-card-text>
+                  <nuxt-link
+                    :to="{ name: 'movieid', params: { movieid: movie.id } }"
+                  >
+                    <v-btn color="primary">Read More</v-btn>
+                  </nuxt-link>
+                </v-card-text>
+                <!-- <span id="vote">3.5</span> -->
+                <span id="vote">{{ movie.vote_average }}</span>
+              </v-card>
+            </v-col>
+          </v-row>
+        </div>
+        <div v-else>
+          <v-row>
+            <v-col
+              xl="3"
+              md="4"
+              sm="6"
+              xs="12"
+              v-for="movie in searchMoive"
+              :key="movie.id"
+            >
+              <v-card>
+                <!-- <v-img
                 class="image"
                 height="250"
-                :src="`https://www.themoviedb.org/t/p/w500/${movie.poster_path}`"
-              >
-              </v-img>
-              <v-card-title>
-                <!-- <h4>Moive title</h4> -->
-                <!-- <h4>{{ movie.title }}</h4> -->
-                <h4>{{ movie.title.slice(0,10) }} {{movie.title.length > 10 ? '.....' : ''}}</h4>
-              </v-card-title>
-              <v-card-text>
-                <nuxt-link
-                  :to="{ name: 'movieid', params: { movieid: movie.id } }"
+                src="https://images.unsplash.com/photo-1512070853659-f08f0efae470?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1070&q=80"
+              ></v-img> -->
+                <v-img
+                  class="image"
+                  height="250"
+                  :src="`https://www.themoviedb.org/t/p/w500/${movie.poster_path}`"
                 >
-                  <v-btn color="primary">Read More</v-btn>
-                </nuxt-link>
-              </v-card-text>
-              <!-- <span id="vote">3.5</span> -->
-              <span id="vote">{{ movie.vote_average }}</span>
-            </v-card>
-          </v-col>
-        </v-row>
+                </v-img>
+                <v-card-title>
+                  <!-- <h4>Moive title</h4> -->
+                  <!-- <h4>{{ movie.title }}</h4> -->
+                  <h4>
+                    {{ movie.title.slice(0, 10) }}
+                    {{ movie.title.length > 10 ? "....." : "" }}
+                  </h4>
+                </v-card-title>
+                <v-card-text>
+                  <nuxt-link
+                    :to="{ name: 'movieid', params: { movieid: movie.id } }"
+                  >
+                    <v-btn color="primary">Read More</v-btn>
+                  </nuxt-link>
+                </v-card-text>
+                <!-- <span id="vote">3.5</span> -->
+                <span id="vote">{{ movie.vote_average }}</span>
+              </v-card>
+            </v-col>
+          </v-row>
+        </div>
       </v-container>
     </div>
   </div>
@@ -60,8 +118,8 @@ export default {
     return {
       movies: [],
       isLoading: false,
-      searchMoive:[],
-      search:''
+      searchMoive: [],
+      search: "",
     };
   },
   methods: {
@@ -70,25 +128,36 @@ export default {
       // https://api.themoviedb.org/3/movie/550?api_key=d3de84b80bb26bc66fdf225d5f5593fd
       // https://www.themoviedb.org/movie/550-fight-club?language=th
       // https://api.themoviedb.org/3/movie/550?api_key=d3de84b80bb26bc66fdf225d5f5593fd&language=th
+      // "https://api.themoviedb.org/3/movie/now_playing?api_key=d3de84b80bb26bc66fdf225d5f5593fd&language=en-US&page=1"
       const response = await axios.get(
-        "https://api.themoviedb.org/3/movie/now_playing?api_key=d3de84b80bb26bc66fdf225d5f5593fd&language=th&page=1"
+        "https://api.themoviedb.org/3/movie/now_playing?api_key=d3de84b80bb26bc66fdf225d5f5593fd&language=th-TH&page=1"
       );
       // console.log(response);
       console.log(response.data.results);
       this.movies = response.data.results;
       this.isLoading = false;
     },
-      async searchMovies() {
+    async searchMovies() {
       this.isLoading = true;
       const response = await axios.get(
-        "https://api.themoviedb.org/3/movie/now_playing?api_key=d3de84b80bb26bc66fdf225d5f5593fd&language=th&page=1"
+        `https://api.themoviedb.org/3/search/movie/?api_key=d3de84b80bb26bc66fdf225d5f5593fd&language=th-TH&page=1&query=${this.search}`
       );
       this.searchMoive = response.data.results;
       this.isLoading = false;
     },
+    clearSearch() {
+      this.search = "";
+      this.searchMovie = [];
+    },
   },
   async fetch() {
-    await this.getMovies();
+    // await this.getMovies();
+    if (this.search === "") {
+      await this.getMovies();
+    }
+    if (this.search !== "") {
+      await this.searchMovies();
+    }
   },
 };
 </script>
@@ -110,10 +179,10 @@ export default {
   justify-content: center;
   margin: 50px 0px;
 }
-.search{
+.search {
   display: flex;
   width: 50%;
-  margin:20px 0px;
+  margin: 20px 0px;
   align-items: center;
 }
 </style>
